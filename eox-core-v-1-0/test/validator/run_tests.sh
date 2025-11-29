8b69331351e1b47d89fbd000f068128986f069d2
tschmidtb51 <65305130+tschmidtb51@users.noreply.github.com>
2025-11-29 01:12:34 +0100

    Tests
    - addresses parts of oasis-tcs/openeox#131
    - add draft automation scripts
    - add testcases schema
    - add GitHub workflows

---
+++ eox-core-v-1-0/test/validator/run_tests.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ORIG_SCHEMA=eox-core-v-1-0/schema/eox-core.json
+META_SCHEMA=eox-core-v-1-0/schema/meta.json
+VALIDATOR=eox-core-v-1-0/test/validator.py
+STRICT_GENERATOR=eox-core-v-1-0/test/generate_strict_schema.py
+TESTPATH=eox-core-v-1-0/test/validator/data/$1/*.json
+EXCLUDE=''
+EXCLUDE_LEAP=''
+
+FAIL=0
+
+# go to root of git repository
+cd `dirname $0`/../../..
+
+validate() {
+  printf "%s" "Testing file $1 against schema ${SCHEMA} ... "
+  if python3 $VALIDATOR $SCHEMA $1 ${META_SCHEMA}; then
+    printf "%s\n" SUCCESS
+  else
+    printf "%s\n" FAILED
+    FAIL=1
+  fi
+
+}
+
+test_all() {
+  for i in $(ls -1 ${TESTPATH} | grep -Ev "${EXCLUDE}" | grep -Ev "${EXCLUDE_LEAP}")
+  do
+    validate $i
+  done
+}
+
+test_all_strict() {
+  for i in $(ls -1 ${TESTPATH} | grep -Ev "${EXCLUDE}" | grep -Ev "${EXCLUDE_LEAP}" )
+  do
+    validate $i
+  done
+}
+
+SCHEMA=$ORIG_SCHEMA
+test_all
+
+
+printf "%s" "Generating strict schema ... "
+mkdir -p ${STRICT_BUILD}
+python3 "${STRICT_GENERATOR}" "${ORIG_SCHEMA}" > "${CSAF_STRICT_SCHEMA}"
+printf "%s\n" "done"
+
+SCHEMA=${CSAF_STRICT_SCHEMA}
+test_all_strict
+
+exit ${FAIL}
